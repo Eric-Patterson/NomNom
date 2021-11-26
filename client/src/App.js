964cd0c02c4cac3600e3ade02d3276f1441cd240
Eric Patterson <68718328+IamTissue@users.noreply.github.com>
2021-11-25 20:20:36 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import SuperheroDetail from './components/SuperheroDetail';
-import SuperheroList from './components/SuperheroList';
-import DonationForm from './components/forms/DonationForm';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import DonationForm from "./components/forms/DonationForm";
 
-import './App.css';
-
+import "./App.css";
 
 function App() {
-
-  const [selectedSuperheroId, setSelectedSuperheroId] = useState()
-
   return (
     <div className="App">
-      <DonationForm/>
-      {/* { selectedSuperheroId ? 
-          <div>
-            <button onClick={() => setSelectedSuperheroId(undefined)}>Go Back</button>
-            <SuperheroDetail  superheroId={selectedSuperheroId}/> 
-          </div>
-        : 
-        <SuperheroList setSelectedSuperheroId={setSelectedSuperheroId} />  
-      } */}
-
-      { !selectedSuperheroId && <SuperheroList setSelectedSuperheroId={setSelectedSuperheroId} />  }
-      { selectedSuperheroId && <div>
-            <button onClick={() => setSelectedSuperheroId(undefined)}>Go Back</button>
-            <SuperheroDetail  superheroId={selectedSuperheroId}/> 
-          </div>
-      }
+      <Navbar />
+      <DonationForm />
     </div>
   );
 }
---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        utl: '#',
+        cName: 'nav-links' 
+    },
+    {
+        title: 'Services',
+        utl: '#',
+        cName: 'nav-links' 
+    },
+    {
+        title: 'Products',
+        utl: '#',
+        cName: 'nav-links' 
+    },
+    {
+        title: 'Contact',
+        utl: '#',
+        cName: 'nav-links' 
+    },
+    {
+        title: 'Sign up',
+        utl: '#',
+        cName: 'nav-links-mobile' 
+    },
+]
