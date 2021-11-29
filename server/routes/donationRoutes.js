585c07ea841817cbe53b39aab4329d3fa3703e16
Your Name <hannahlo_26@hotmail.com>
2021-11-28 20:50:19 -0700

    more updates

--- server/routes/donationRoutes.js
+++ server/routes/donationRoutes.js
@@ -2,20 +2,18 @@
 const express = require('express')
 const router = express.Router()
 
-// const superheroModel = require('../models/superhero')
+const donationModel = require ('../models/donation')
 
-const donationModel = require ('../models/donations')
+router.get('/donation', async (req, res) => {
+    let donationList = await donationModel.listDonations()
+    res.send(donationsList)
+})
 
-// router.get('/superhero', async (req, res) => {
-//     let superheroList = await superheroModel.listSuperheros()
-//     res.send(superheroList)
-// })
-
-// router.get('/superhero/:id', async (req, res) => {
-//     let id =req.params.id
-//     let superhero = await superheroModel.findById(id)
-//     res.send(superhero)
-// })
+router.get('/donation/:id', async (req, res) => {
+    let id =req.params.id
+    let donation = await donationModel.findById(id)
+    res.send(donation)
+})
 
 router.post('/donationForm', async (req, res) => {
     let newDonation = req.body
