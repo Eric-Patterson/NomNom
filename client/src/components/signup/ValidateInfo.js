5a003a6c4009d4b30fb057958014901c7d58fc7e
Ali Ollaik <ollaikali@gmail.com>
2021-12-03 18:47:14 -0700

    Fixed a small error

--- client/src/components/signup/ValidateInfo.js
+++ client/src/components/signup/ValidateInfo.js
@@ -1,5 +1,7 @@
 export default function validateInfo(values) {
     let errors = {}
 
-    if(!values.username.trim) //If username is not in place, there will be this error. Trim to get rid of white space around
+    if(!values.username.trim) {
+        
+    } //If username is not in place, there will be this error. Trim to get rid of white space around
 }
