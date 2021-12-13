081ae36021817250d1b9b53a82000586a892ee70
Eric Patterson <ericpattersonwork@gmail.com>
2021-12-13 10:16:41 -0700

    details page should be working now with login api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,61 +1,61 @@
-const User = require("../models/users");
+// const User = require("../models/users");
 
-// renders the register form
-module.exports.renderRegister = (req, res) => {
-  res.render("register");
-};
+// // renders the register form
+// module.exports.renderRegister = (req, res) => {
+//   res.render("register");
+// };
 
-// registers the user
-module.exports.register = async (req, res, next) => {
-  // res.send(req.body)
-  // We try catch this, with e.message it will flash the error message to the user and then redirect them back to the page
-  try {
-    // const newUser = new User(req.body);
-    // await newUser.save();
-    const { email, username, password } = req.body;
-    // const currentLocation =
-    const user = new User({ email, username });
-    // this registers the user, hashes the password
-    const registeredUser = await User.register(user, password);
-    // we login the user that was created, and redirect them to /gyms with flashed message
-    req.login(registeredUser, (err) => {
-      if (err) return next(err);
+// // registers the user
+// module.exports.register = async (req, res, next) => {
+//   // res.send(req.body)
+//   // We try catch this, with e.message it will flash the error message to the user and then redirect them back to the page
+//   try {
+//     // const newUser = new User(req.body);
+//     // await newUser.save();
+//     const { email, username, password } = req.body;
+//     // const currentLocation =
+//     const user = new User({ email, username });
+//     // this registers the user, hashes the password
+//     const registeredUser = await User.register(user, password);
+//     // we login the user that was created, and redirect them to /gyms with flashed message
+//     req.login(registeredUser, (err) => {
+//       if (err) return next(err);
 
-      //we can flash the user a message below.
-      //   req.flash("");
+//       //we can flash the user a message below.
+//       //   req.flash("");
 
-      // need to fix redirect
-      //   res.redirect("/");
-    });
-  } catch (e) {
-    req.flash("error", e.message);
+//       // need to fix redirect
+//       //   res.redirect("/");
+//     });
+//   } catch (e) {
+//     req.flash("error", e.message);
 
-    // need to fix redirect
-    // res.redirect("");
-  }
-};
+//     // need to fix redirect
+//     // res.redirect("");
+//   }
+// };
 
-// renders the login form
-module.exports.renderLogin = (req, res) => {
-  res.render("login");
-};
+// // renders the login form
+// module.exports.renderLogin = (req, res) => {
+//   res.render("login");
+// };
 
-// Logs in the user
-module.exports.login = (req, res) => {
-  // req.flash("success", "Welcome back!");
-  // either we redirect to the session the user was trying to access to before login
-  // if no session was trying to be accessed the user will be redirected to the homepage '/gyms'
-  const redirectUrl = req.session.returnTo || "/";
-  delete req.session.returnTo;
-  res.redirect(redirectUrl);
-};
+// // Logs in the user
+// module.exports.login = (req, res) => {
+//   // req.flash("success", "Welcome back!");
+//   // either we redirect to the session the user was trying to access to before login
+//   // if no session was trying to be accessed the user will be redirected to the homepage '/gyms'
+//   const redirectUrl = req.session.returnTo || "/";
+//   delete req.session.returnTo;
+//   res.redirect(redirectUrl);
+// };
 
-// logs out the user
-module.exports.logout = (req, res) => {
-  // req.logout(); is a passport function that logs you out of their login middleware
-  req.logout();
-  // req.flash("success", "You have been logged out!");
+// // logs out the user
+// module.exports.logout = (req, res) => {
+//   // req.logout(); is a passport function that logs you out of their login middleware
+//   req.logout();
+//   // req.flash("success", "You have been logged out!");
 
-  // need to fix redirect
-  //   res.redirect("/start");
-};
+//   // need to fix redirect
+//   //   res.redirect("/start");
+// };
--- server/server.js
+++ server/server.js
@@ -5,17 +5,16 @@
 
 const express = require("express");
 const donationRoutes = require("./routes/donationRoutes");
-const loginRoutes = require("./routes/login")
-const cors = require("cors")
+const loginRoutes = require("./routes/login");
+const cors = require("cors");
 const app = express();
 const port = 5000;
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", donationRoutes);
-app.use("/api", loginRoutes)
+app.use("/api", loginRoutes);
 app.use("/", express.static("../client/build"));
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
