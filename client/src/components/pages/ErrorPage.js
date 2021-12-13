6d600c86bfee80d40b78c6a5e5dd3d4cd7da5e18
Snoobana <hannahlo_26@hotmail.com>
2021-12-13 11:30:17 -0700

    hehehe

--- client/src/components/pages/ErrorPage.js
+++ client/src/components/pages/ErrorPage.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import "./ErrorPage.css"
 
 function ErrorPage() {
   return (
     <p>
-      Page not available, please go back to <Link to="/">Home</Link>
+      <h3>Page not available, please go back to <Link to="/">Home</Link></h3>
+
+      <div className="centerImg">
+        <img src={process.env.PUBLIC_URL + "/cat.jpeg"} />
+      </div>
+
     </p>
   );
 }
