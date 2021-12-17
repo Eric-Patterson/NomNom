fbb586e67834c7b9ccdf23dbc815e2fd307213e4
Eric Patterson <ericpattersonwork@gmail.com>
2021-12-16 18:20:16 -0700

    backend stuff

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,39 +1,40 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
+// const express = require("express");
+// const router = express.Router();
+// const passport = require("passport");
+// // const catchAsync = require("../utils/catchAsync");
 
-const users = require("../controllers/user");
+// const users = require("../controllers/user");
 
-router
-  .route("/register")
-  .get(users.renderRegister)
-  .post(catchAsync(users.register));
+// router
+//   .route("/register")
+//   .get(users.renderRegister)
+//   .post(catchAsync(users.register));
 
-// renders the register form
-// router.get('/register', users.renderRegister);
-// registers the user
-// router.post('/register', catchAsync(users.register));
+// // renders the register form
+// // router.get('/register', users.renderRegister);
+// // registers the user
+// // router.post('/register', catchAsync(users.register));
 
-router
-  .route("/login")
-  .get(users.renderLogin)
-  .post(
-    passport.authenticate("local", {
-      failureFlash: true,
-      failureRedirect: "/login",
-    }),
-    users.login
-  );
+// router
+//   .route("/login")
+//   .get(users.renderLogin)
+//   .post(
+//     passport.authenticate("local", {
+//       failureFlash: true,
+//       failureRedirect: "/login",
+//     }),
+//     users.login
+//   );
 
-// renders the login form
-// router.get('/login', users.renderLogin)
-// Logs in the user
-// passport comes with their own authentication middleware.
-// using this middleware we authentiate the local,
-// here are the options, if there is a failure redirect to local
-// router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+// // renders the login form
+// // router.get('/login', users.renderLogin)
+// // Logs in the user
+// // passport comes with their own authentication middleware.
+// // using this middleware we authentiate the local,
+// // here are the options, if there is a failure redirect to local
+// // router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
 
-// logs out the user
-router.get("/logout", users.logout);
+// // logs out the user
+// router.get("/logout", users.logout);
 
-module.exports = router;
+// module.exports = router;
